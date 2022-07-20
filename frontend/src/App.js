59ad251a52c8291d25b585bad81630fe255274a4
Aakash Chaudhary <aakash.199303185@muj.manipal.edu>
2022-07-21 05:08:22 +0530

    added context and provider to connect to blockchain, implemented in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import React, {useState } from "react";
+import React, { useState, useContext } from "react";
 import Login from "./containers/Login";
 import Home from "./containers/Home";
-import 'normalize.css';
-import './styles/styles.css';
+import "normalize.css";
+import "./styles/styles.css";
+import { TransactionContext } from "./context/TransactionContext";
 
-
-function App() {
+const App = () => {
+  const { connectWallet } = useContext(TransactionContext);
   const [loggedIn, setLoggedIn] = useState(false);
-  return loggedIn? <Home /> : <Login setLoggedIn={setLoggedIn} />
-}
+  return loggedIn ? (
+    <Home />
+  ) : (
+    <Login setLoggedIn={setLoggedIn} connectWallet={connectWallet} />
+  );
+};
 
 export default App;
--- frontend/src/components/SignInButton.jsx
+++ frontend/src/components/SignInButton.jsx
@@ -1,42 +1,40 @@
-import React, { useState } from 'react'
-import MM from '../assets/mm.png'
-import Loading from './Loading';
+import React, { useState } from "react";
+import MM from "../assets/mm.png";
+import Loading from "./Loading";
 
 const ButtonText = () => {
   return (
-    <div className='row'>
-      <img src={MM} alt="" className='mm'/>
-      <p className='btn-text'>Sign in with Metamask</p>
+    <div className="row">
+      <img src={MM} alt="" className="mm" />
+      <p className="btn-text">Sign in with Metamask</p>
     </div>
   );
-}
+};
 
-
-const SignInButton = ({setLoggedIn, setError}) => {
+const SignInButton = ({ setLoggedIn, setError, connectWallet }) => {
   const [loading, setLoading] = useState(false);
 
   return (
     <div
-    className= {`btn`}
-    onClick={async () =>  { 
-      setLoading(true);
-      setTimeout(() => {
-        try {
-          if(window.ethereum.isConnected() && window.ethereum.isMetaMask) {
-            setLoggedIn(true);
-            setError(false);
+      className={`btn`}
+      onClick={async () => {
+        setLoading(true);
+        setTimeout(() => {
+          try {
+            if (connectWallet()) {
+              setLoggedIn(true);
+              setError(false);
+            }
+          } catch (e) {
+            setLoading(false);
+            setError(true);
           }
-        }
-        catch(e) {
-          setLoading(false);
-          setError(true);
-        }
-      }, 1000);
-    }}
+        }, 1000);
+      }}
     >
-        {loading? <Loading loading={loading}/> : <ButtonText />}
-      </div>
-  )
-}
+      {loading ? <Loading loading={loading} /> : <ButtonText />}
+    </div>
+  );
+};
 
-export default SignInButton
+export default SignInButton;
