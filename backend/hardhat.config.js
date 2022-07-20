2f63a24aeda7ed868901543266488d9d67860a6a
Aakash Chaudhary <aakash.199303185@muj.manipal.edu>
2022-07-20 05:36:01 +0530

    updated backend

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -3,7 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-  paths: {
-    artifacts: "./src/artifacts",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/qe7sfj2YYNQZE7n3VcKnB7AqdlWTV-Ul",
+      accounts: [
+        "5ad2d795dfa376f88acf7d7240ac3420355561ec3f6571b1a9a29ae33802d879",
+      ],
+    },
   },
 };
