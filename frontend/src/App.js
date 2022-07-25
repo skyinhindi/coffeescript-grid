d8712fc1a6adefbb1e99a2dd8379c7663eceb54b
Aakash Chaudhary <aakash.199303185@muj.manipal.edu>
2022-07-26 04:34:24 +0530

    added moralis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,47 @@
-import React, {useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useMoralis } from "react-moralis";
+
 import Login from "./containers/Login";
 import Home from "./containers/Home";
-import 'normalize.css';
-import './styles/styles.css';
-
+import "normalize.css";
+import "./styles/styles.css";
 
 function App() {
-  
+  const {
+    authenticate,
+    isAuthenticated,
+    isAuthenticating,
+    user,
+    account,
+    logout,
+  } = useMoralis();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated]);
+
+  const login = async () => {
+    if (!isAuthenticated) {
+      await authenticate({ signingMessage: "Log in using Moralis" })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user.get("ethAddress"));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  };
+
+  const logOut = async () => {
+    await logout();
+    console.log("logged out");
+  };
+
   const [loggedIn, setLoggedIn] = useState(false);
-  return !loggedIn? <Home /> : <Login setLoggedIn={setLoggedIn} />
+  return loggedIn ? <Home /> : <Login setLoggedIn={setLoggedIn} />;
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { MoralisProvider } from "react-moralis";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <MoralisProvider serverUrl="https://xxxxx/server" appId="YOUR_APP_ID">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MoralisProvider>
+  </React.StrictMode>
 );
-
-
-
